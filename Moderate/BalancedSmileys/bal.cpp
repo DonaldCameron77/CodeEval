6a30c55565e44b1d0385aed0f0ef05f66102f8e7
Donald Cameron <cameron.donald77@gmail.com>
2014-08-19 20:35:23 -0600

    yet more minor cleanup

--- Moderate/BalancedSmileys/bal.cpp
+++ Moderate/BalancedSmileys/bal.cpp
@@ -16,7 +16,8 @@ message has balanced parentheses if it consists of one of the following:
 
 - An empty string "" 
 - One or more of the following characters: 'a' to 'z', ' ' (a space) or ':' (a colon) 
-- An open parenthesis '(', followed by a message with balanced parentheses, followed by a close parenthesis ')'.
+- An open parenthesis '(', followed by a message with balanced parentheses,
+  :f followed by a close parenthesis ')'.
 - A message with balanced parentheses followed by another message with balanced parentheses. 
 - A smiley face ":)" or a frowny face ":(" 
 
