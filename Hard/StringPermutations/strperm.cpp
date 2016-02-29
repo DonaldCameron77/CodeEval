0c4c35470b3ab8c4021c928860e00abd75680a91
Donald Cameron <dcc1079@gmail.com>
2016-02-28 19:38:56 -0700

    fixed test file + minor cosmetic change

--- Hard/StringPermutations/strperm.cpp
+++ Hard/StringPermutations/strperm.cpp
@@ -3,11 +3,14 @@ STRING PERMUTATIONS - CodeEval Hard challenge
 
 CHALLENGE DESCRIPTION:
 
-Write a program to print out all the permutations of a string in alphabetical order. We consider that digits < upper case letters < lower case letters. The sorting should be performed in ascending order.
+Write a program to print out all the permutations of a string in
+alphabetical order. We consider that digits < upper case letters < lower
+case letters. The sorting should be performed in ascending order.
 
 INPUT SAMPLE:
 
-Your program should accept as its first argument a path to a file containing an input string, one per line. E.g.
+Your program should accept as its first argument a path to a file
+containing an input string, one per line. E.g.
 
 hat
 abc
@@ -15,7 +18,8 @@ Zu6
 
 OUTPUT SAMPLE:
 
-Print to stdout, permutations of the string, comma separated, in alphabetical order. E.g.
+Print to stdout, permutations of the string, comma separated, in
+alphabetical order. E.g.
 
 aht,ath,hat,hta,tah,tha
 abc,acb,bac,bca,cab,cba
@@ -25,7 +29,7 @@ abc,acb,bac,bca,cab,cba
 
 #include <iostream>
 #include <fstream>
-// #include <string>
+#include <string>
 #include <algorithm>
 
 using namespace std;
@@ -38,40 +42,41 @@ void cswap(string & s, unsigned i, unsigned j) {
 
 void permute_and_print(string s, unsigned start, bool & first)
 {
-	if (start == s.size() - 1) {
-	    if (first) {
-	        first = false;
-	        cout << s;
-	    }
-	    else {
-	         cout << ',' << s;
-	    }
+    if (start == s.size() - 1) {
+	if (first) {
+	    first = false;
+	    cout << s;
 	}
 	else {
-		for (unsigned i = start; i < s.size(); ++i) {
-			permute_and_print(s, start + 1, first);
-			cswap(s, start, i+1); // useless on last trip of loop when we'll just exit
-		}
+	     cout << ',' << s;
 	}
+    }
+    else {
+	for (unsigned i = start; i < s.size(); ++i) {
+	    permute_and_print(s, start + 1, first);
+	    cswap(s, start, i+1); // useless on last trip of loop when we'll just exit
+	}
+    }
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+    if (argc != 2) {
+	cout << "usage: " << argv[0] << "<filename>\n";
+	return 1;
+    }
 
-	if (argc != 2) {
-    		cout << "usage: " << argv[0] << "<filename>\n";
-    		return 1;
-	}
+    ifstream ifs(argv[1]);
+    string line;
 
-	ifstream ifs(argv[1]);
-	string line;
-
-	while (getline(ifs, line))
-	{
-		sort(line.begin(), line.end());
-		bool first = true;
-		permute_and_print(line, 0, first);
-		cout << endl;
-		
-	}
+    while (getline(ifs, line))
+    {
+	sort(line.begin(), line.end());
+	bool first = true;
+	permute_and_print(line, 0, first);
+	cout << endl;
+	    
+    }
 }
 
+// EOF
